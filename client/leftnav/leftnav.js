f0bb595472233a24a596c4e8af0badc0bdb1ec28
Mahmoud Kida <mahmoudkida@gmail.com>
2016-02-27 22:24:18 +0300

    adding the right column
    -boards section
    -download mobile app section
    -ads section

---
+++ client/leftnav/leftnav.js
@@ -0,0 +1,11 @@
+Template.leftnav.rendered = function() {
+    $(".scroll-this").niceScroll({
+        cursorcolor:"#d1d1d3",
+        autohidemode:false,
+        cursorborder :"1px solid #c2c2c2",
+        cursorwidth :"7px",
+        cursorborderradius :"0px",
+        railpadding:{top:0,right:10,left:0,bottom:0}
+    });
+    
+};
